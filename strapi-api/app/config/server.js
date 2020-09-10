7f01bd14883d0a10d5c3d1a97ba9fd26b62c16e4
Leandro Soares <lsoares199658@gmail.com>
2020-09-10 11:12:38 -0300

    initial commit

---
+++ strapi-api/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0041c270acce19f8a97b1716c636efc6'),
+    },
+  },
+});
---
+++ strapi-api/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cea27bcd-e10d-4f94-b5b3-21edebfcc914'
+};
